547fa6d80d7fc98bf9e2b95aa28a77b345909579
Wendly Saintil <swendlex2014@gmail.com>
2020-07-21 02:24:08 +0000

    enable group replication in database

--- database.sh
+++ database.sh
@@ -3,60 +3,72 @@
 source /etc/wspecs/global.conf
 source /etc/wspecs/functions.sh
 
-DB_ROLE="${DB_ROLE:-master}"
-DB_SERVER_ID="${DB_SERVER_ID:-1}"
-DB_BIND_ADDRESS="${DB_BIND_ADDRESS:-$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')}"
-MASTER_USER="${MASTER_USER:-slave_user}"
-MASTER_HOST="${MASTER_HOST:-}"
-MASTER_PASSWORD="${MASTER_PASSWORD:-}"
-MASTER_LOG="${MASTER_LOG:-}"
-MASTER_POSITION="${MASTER_POSITION:-}"
-
-install_once mysql-server
-
-if [ -f "~/.my.cnf" ]; then
-  echo MySQL login is already configured
-else
-  echo "Setting up MYSQL login"
-  NEW_PASSWORD=$(openssl rand -base64 36 | tr -d "=+/" | cut -c1-32)
-  mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$NEW_PASSWORD')"
-  mysql -e "DELETE FROM mysql.user WHERE User=''"
-  mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-  mysql -e "DROP DATABASE IF EXISTS test"
-  mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
-  mysql -e "FLUSH PRIVILEGES"
-
-  echo "Creating mysql config"
-  cat > ~/.my.cnf <<EOL
-[mysql]
-user=root
-password=$NEW_PASSWORD
-EOL
-  chmod 0600 ~/.my.cnf
-fi
-
-add_mysql_config() {
-  cp ./my.cnf /etc/mysql/my.cnf
-  add_config server-id=$DB_SERVER_ID /etc/mysql/my.cnf
+function join() {
+    # $1 is return variable name
+    # $2 is sep
+    # $3... are the elements to join
+    local retname=$1 sep=$2 ret=$3
+    shift 3 || shift $(($#))
+    printf -v "$retname" "%s" "$ret${@/#/$sep}"
 }
 
-if [ "$DB_ROLE" == "master" ]; then
-  add_mysql_config
-  add_config bind-address=$DB_BIND_ADDRESS /etc/mysql/my.cnf
+CONFIG_FILE=${CONFIG_FILE:-/etc/mysql/my.cnf}
+UNIQUE_ID="${UNIQUE_ID:-$(uuidgen)}"
+CURRENT_IP=$(curl 169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address && echo)
+SERVER_ID=1
 
-  if [ ! -f "~/.db.replication.passport" ]; then
-    REPLICATION_PASSPORT=$(openssl rand -base64 36 | tr -d "=+/" | cut -c1-32)
-    add_config passport=$REPLICATION_PASSPORT ~/.db.replication.passport
-  fi
-
-  mkdir -p /var/log/mysql
-  chown -R mysql:mysql /var/log/mysql
-  mysql -e "GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' IDENTIFIED BY '$REPLICATION_PASSPORT'"
-  mysql -e "FLUSH PRIVILEGES;"
-  sudo service mysql restart
-else
-  add_mysql_config
-  mysql -e "CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',MASTER_USER='$MASTER_USER', MASTER_PASSWORD='$MASTER_PASSWORD', MASTER_LOG_FILE='$MASTER_LOG', MASTER_LOG_POS=$MASTER_POSITION"
-  mysql -e "START SLAVE"
-  sudo service mysql restart
+if [ ! -z "${START_FIRST_NODE+x}" ]; then
+  mysql -e "SET GLOBAL group_replication_bootstrap_group=ON;"
+  mysql -e "START GROUP_REPLICATION;"
+  mysql -e "SET GLOBAL group_replication_bootstrap_group=OFF;"
+  mysql -e "SELECT * FROM performance_schema.replication_group_members;"
+  echo Node Started
+  exit
 fi
+
+if [ ! -z "${START_NODE+x}" ]; then
+  mysql -e "START GROUP_REPLICATION;"
+  mysql -e "SELECT * FROM performance_schema.replication_group_members;"
+  echo Node Started
+  exit
+fi
+
+if [ ! -z "${FINISH_INSTALLATION+x}" ]; then
+  sed -i "#start_on_boot = OFF#start_on_boot = ON#" $CONFIG_FILE
+  systemctl restart mysql
+  echo Installation completed
+  exit
+fi
+
+join IPS , ${DATABASE_SERVERS}
+join SEED :33061, ${DATABASE_SERVERS}
+SEEDS=${SEED}:33061
+
+
+# Store unique config
+add_config UNIQUE_ID=$UNIQUE_ID /etc/wspecs/global.conf
+
+install_once mysql-server
+sed "s#SERVER_ID#$SERVER_ID#" my.cnf > $CONFIG_FILE
+sed -i "s#UNIQUE_ID#$UNIQUE_ID#" $CONFIG_FILE
+sed -i "s#CURRENT_IP#$CURRENT_IP#" $CONFIG_FILE
+sed -i "s#IPS#$IPS#" $CONFIG_FILE
+sed -i "s#SEEDS#$SEEDS#" $CONFIG_FILE
+
+systemctl restart mysql
+
+sudo ufw allow from 10.138.0.0/16 to any port 33061
+sudo ufw allow from 10.138.0.0/16 to any port 3306
+
+mysql -e "SET SQL_LOG_BIN=0;"
+mysql -e "CREATE USER IF NOT EXISTS 'repl'@'%' IDENTIFIED BY '${GROUP_PASSWORD}' REQUIRE SSL;"
+mysql -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "SET SQL_LOG_BIN=1;"
+mysql -e "CHANGE MASTER TO MASTER_USER='repl', MASTER_PASSWORD='${GROUP_PASSWORD}' FOR CHANNEL 'group_replication_recovery';"
+mysql -e "SHOW PLUGINS;"
+if [[ $(mysql -e "SHOW PLUGINS;" | head -c1 | wc -c) -eq 0 ]]; then 
+  mysql -e "INSTALL PLUGIN group_replication SONAME 'group_replication.so';"
+fi
+mysql -e "SHOW PLUGINS;"
+
